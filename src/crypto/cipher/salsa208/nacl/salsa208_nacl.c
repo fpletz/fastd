e31253760be07fe0c8d490b4a93017857e015b9a
Franz Pletz <fpletz@fnordicwalking.de>
2015-01-19 10:26:29 +0100

    Add Salsa 20/8 cipher

---
+++ src/crypto/cipher/salsa208/nacl/salsa208_nacl.c
@@ -0,0 +1,73 @@
+/*
+  Copyright (c) 2015, Matthias Schiffer <mschiffer@universe-factory.net>
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/**
+   \file
+
+   The Salsa20/8 implementation from NaCl
+*/
+
+
+#include "../../../../alloc.h"
+#include "../../../../crypto.h"
+
+#include <crypto_stream_salsa208.h>
+
+
+/** The cipher state */
+struct fastd_cipher_state {
+	uint8_t key[crypto_stream_salsa208_KEYBYTES];	/**< The encryption key */
+};
+
+
+/** Initializes the cipher state */
+static fastd_cipher_state_t * salsa208_init(const uint8_t *key) {
+	fastd_cipher_state_t *state = fastd_new(fastd_cipher_state_t);
+	memcpy(state->key, key, crypto_stream_salsa208_KEYBYTES);
+
+	return state;
+}
+
+/** XORs data with the Salsa20/8 cipher stream */
+static bool salsa208_crypt(const fastd_cipher_state_t *state, fastd_block128_t *out, const fastd_block128_t *in, size_t len, const uint8_t *iv) {
+	crypto_stream_salsa208_xor(out->b, in->b, len, iv, state->key);
+	return true;
+}
+
+/** Frees the cipher state */
+static void salsa208_free(fastd_cipher_state_t *state) {
+	if (state) {
+		secure_memzero(state, sizeof(*state));
+		free(state);
+	}
+}
+
+
+/** The nacl salsa208 implementation */
+const fastd_cipher_t fastd_cipher_salsa208_nacl = {
+	.init = salsa208_init,
+	.crypt = salsa208_crypt,
+	.free = salsa208_free,
+};
